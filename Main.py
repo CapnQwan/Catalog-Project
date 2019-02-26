c09c62105cad8dd7fe467933fa34ca3cd091dea3
CapnQwan <q.p1453@gmail.com>
2019-02-26 14:00:06 +1300

    added a route for it to render the template at

--- Main.py
+++ Main.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+@app.route('/corvus')
+def homepage():
+	return render_template('header.html')
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
