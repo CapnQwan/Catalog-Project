93fcf87c72bf74d405e5e0f9eecc6b62ee4a0ce0
CapnQwan <q.p1453@gmail.com>
2019-03-04 19:05:33 +1300

    Added the ability to sign up

--- DB_setup.py
+++ DB_setup.py
@@ -2,9 +2,12 @@ import random, string
 from sqlalchemy.ext.declarative import declarative_base
 from passlib.apps import custom_app_context as pwd_context
 from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from insdangerous import (TimedJSONWebSignatureSerializer as serializer, BadSignature, SignatureExpired)
 
 
 Base = declarative_base()
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
 
 
 class User(Base):
@@ -20,6 +23,26 @@ class User(Base):
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
 
+    def generate_auth_token(self, expiration=600):
+    	s = Serializer(secret_key, expires_in = expiration)
+    	return s.dumps({'id': self.id })
+
+    @staticmethod
+    def verify_auth_token(token):
+    	s = Serializer(secret_key)
+    	try:
+    		data = s.loads(token)
+    	except SignatureExpired:
+    		#Valid Token, but expired
+    		return None
+    	except BadSignature:
+    		#Invalid Token
+    		return None
+    	user_id = data['id']
+    	return user_id
+
 
 engine = create_engine('sqlite:///Catalog.db')
+
+
 Base.metadata.create_all(engine)
--- Main.py
+++ Main.py
@@ -1,19 +1,50 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from DB_setup import Base, User
+from flask.ext.httpauth import HTTPBasicAuth
+
 
 app = Flask(__name__)
+
+
+engine = create_engine('sqlite:///Catalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 @app.route('/')
 @app.route('/Corvus')
 def homepage():
 	return render_template('front_page.html')
 
-@app.route('/login')
+
+@app.route('/Corvus/login')
 def Login():
 	return render_template('Login.html')
 
-@app.route('/signup')
+
+@app.route('/Corvus/signup', methods = ['GET', 'POST'])
 def Signup():
-	return render_template('Sign_up.html')
+	if request.method == 'POST':
+		username = request.json.get('username')
+		password = request.json.get('password')
+		email = request.json.get('email')
+		if username in None or password is None:
+			render_template('Signup.html')
+
+		if session.query(User).filter_by(username=username).first is not None:
+			render_template('Signup.html')
+
+		user = User(username = username, email = email)
+		user.hash_password(password)
+		session.add(user)
+		session.commit()
+		return redirect(url_for('/Corvus'))
+	else:
+		return render_template('Sign_up.html')
+
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
